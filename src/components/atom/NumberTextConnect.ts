1e21c43fb0f491121855e3671444675dcdba62ce
easyone1372 <easyone1372@office.duksung.ac.kr>
2024-06-28 00:55:42 +0900

    주석 삭제

--- src/components/atom/NumberTextConnect.ts
+++ src/components/atom/NumberTextConnect.ts
@@ -1,21 +1,3 @@
-//쓰레기 코드
-//강사님이 경악하신 이유가 있음 진심 쓰레기임
-
-// export type connectInfo = [
-//   {
-//     checkNum: 1;
-//     checkMessage: "지각";
-//   },
-//   {
-//     checkNum: 2;
-//     checkMessage: "출석";
-//   },
-//   {
-//     checkNum: 3;
-//     checkMessage: "결석";
-//   }
-// ];
-
 type connectInfoDataProps = {
   checkNum: number;
   checkMessage: string;
